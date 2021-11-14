e9936df7e1f12a69e4f2785f91c92bb22f91d807
dairosierra <94074595+dairosierra@users.noreply.github.com>
2021-11-13 22:14:26 -0500

    Ejemplo gitHub

--- HolaMundo/src/main/java/dairosierra/holamundo/Main.java
+++ HolaMundo/src/main/java/dairosierra/holamundo/Main.java
@@ -19,6 +19,8 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null, "hola");
+        JOptionPane.showMessageDialog(null, "y chao");
+        
     }
     
 }
